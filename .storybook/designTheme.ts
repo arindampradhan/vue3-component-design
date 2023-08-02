783ae46f5abb024dd88f104f1f7cda0c07747812
arindampradhan <arindampradhan10@gmail.com>
2023-08-02 19:27:14 +0530

    rename

--- .storybook/designTheme.ts
+++ .storybook/designTheme.ts
@@ -2,8 +2,7 @@ import { create } from '@storybook/theming'
 
 export default create({
 	base: 'light',
-	brandTitle: 'Zeta | GDS',
-	brandUrl: 'https://gds.zeta.tech',
-	// brandImage: 'https://gds.zeta.tech/images/home/hero-planet-bg.png',
+	brandTitle: 'Component Design System',
+	brandUrl: '',
 	brandTarget: '_self',
 })
